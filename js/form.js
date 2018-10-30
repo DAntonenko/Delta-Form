7b8ce2765a6dab76fae81fe47b18feda057d6c78
Danton <dm.antonenko1977@gmail.com>
2018-10-30 21:09:32 +0300

    Слайдеры готовы
    Всё работает и выглядит согласно макету, за исключением отсутствия изменения цвета полосы при движении ползунка в webkit-браузерах. В принципе решаемо, но сейчас на это нет времени.

--- js/form.js
+++ js/form.js
@@ -7,3 +7,28 @@ form.classList.add('dimensionally-hidden');
 
 formHeader.classList.add('dimensionally-discovered');
 console.log(formHeaderAfter); */
+
+function budgetOutput() {
+  var x = document.getElementById('budget-input').value;
+  if (x == 0) {
+  	var output = 'до 1 млн ₽';
+  } else if (x == 5) {
+  	var output = '1-5 млн ₽';
+  } else {
+  	var output = '> 5 млн ₽';
+  };
+  document.getElementById('budget-output').innerHTML = output;
+};
+
+function deadlineOutput() {
+  var x = document.getElementById('deadline-input').value;
+  if (x == 1) {
+  	var mon = ' месяц';
+  } else if (x > 1 && x < 5) {
+  	var mon = ' месяца';
+  } else {
+  	var mon = ' месяцев';
+  };
+  document.getElementById('deadline-output').innerHTML = x;
+  document.getElementById('months').innerHTML = mon;
+};
