7bd27f7bc479507ba69ef82c59d2866ca9e5d017
Danton <dm.antonenko1977@gmail.com>
2018-10-30 12:02:20 +0300

    Промежуточный результат
    Внешний вид уже более-менее вырисовывается. Пока нет сеток и стилизации некоторых элементов.

---
+++ js/form.js
@@ -0,0 +1,9 @@
+/* var form = document.getElementById('request');
+
+var formHeader = form.querySelector("h1");
+var formHeaderAfter = form.querySelector("h1::after");
+
+form.classList.add('dimensionally-hidden');
+
+formHeader.classList.add('dimensionally-discovered');
+console.log(formHeaderAfter); */
